139b1d2b44e5634ea92b44fd2d3b5d3e06b26db4
Brad Buttineau <113149072+BradB87@users.noreply.github.com>
2024-03-16 13:52:56 -0400

    layout of javaScript

--- smoothie.js
+++ smoothie.js
@@ -1,10 +1,35 @@
 const output = document.querySelector('#app');
 
 class Smoothie{
-    constructor(size,smoType,smoTop, smoExtra){
+    constructor(size, smoMilk , smoType, smoTop, smoExtra){
         this.size=size;
+        this.smoMilk=smoMilk;
         this.smoType=smoType;
         this.smoTop=smoTop;
         this.smoExtra= smoExtra;
     };
+   //show smoothie img
+    serveIt(){
+    const cup = document.createElement("img");
+    let smoothieImg = "images/smoothie.svg";
+
+    }
+    //smoothie sizes
+    let smoothieSize;
+    switch(this.size){
+        case "small":
+            smoothieSize= "100";
+            break;
+        case "medium":
+            smoothieSize= "125";
+            break;
+        case "large":
+            smoothieSize= "150";
+            break;
+        default:
+            smoothieSize= "100";
+    }
+    cup.setAttribute("height", smoothieSize);
+    let description= `A ${this.size}`
+
 }
