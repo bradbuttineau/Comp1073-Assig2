a8be5f55798628fd0e5a1c24eec48455da2f541b
Brad Buttineau <113149072+BradB87@users.noreply.github.com>
2024-03-17 16:02:09 -0400

    js  modify

--- smoothie.js
+++ smoothie.js
@@ -1,17 +1,17 @@
 const output = document.querySelector('#app');
 
 class Smoothie {
-    constructor(size, smoType, smoTop, smoExtra, smoDairy) {
+    constructor(size, smoType, smoTop, smoDairy) {
         this.size = size;
         this.smoType = smoType;
         this.smoTop = smoTop;
-        this.smoExtra = smoExtra;
         this.smoDairy= smoDairy;
     };
     //show smoothie img
     serveIt() {
         const cup = document.createElement("img");
         let smoothieImg = "images/smoothie.svg";
+        let dairy;
         if(this.smoDairy === true){
             dairy= "yes"
         }else{
@@ -35,6 +35,7 @@ class Smoothie {
                 smoothieSize = "100";
         }
         cup.setAttribute("height", smoothieSize);
+       let description = `Your order is complete you ordered: ${this.size}, ${this.smoType}, ${this.smoTop} with ${this.dairy}`
         output.appendChild(cup);
     }
 }
